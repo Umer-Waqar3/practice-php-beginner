c9be70eb931ec1cf0412fd5c593dfd1237d4ce65
umer <umer.waqar@coeus-solutions.de>
2022-01-04 16:56:48 +0500

    date

---
+++ date.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+
+<?php
+echo "The time is " . date("h:i:sa");
+?>
+
+<?php
+date_default_timezone_set("America/New_York");
+echo "The time is " . date("h:i:sa");
+?>
+
+<?php
+$d=mktime(11, 14, 54, 8, 12, 2014);
+echo "Created date is " . date("Y-m-d h:i:sa", $d);
+?>
+
+<?php
+$d=strtotime("10:30pm April 15 2014");
+echo "Created date is " . date("Y-m-d h:i:sa", $d);
+?>
+</body>
+</html>
